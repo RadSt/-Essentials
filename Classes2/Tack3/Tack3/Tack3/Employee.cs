24bf540959ba617cc3040ce1de24c425d46d4258
RadSt <desan1986@gmail.com>
2015-06-14 21:08:03 +0300

    проверка2

--- Classes2/Tack3/Tack3/Tack3/Employee.cs
+++ Classes2/Tack3/Tack3/Tack3/Employee.cs
@@ -5,14 +5,49 @@
         readonly string firstName, secondName;
         private int expirience;
         private string post;
-        
 
+        public string FirstName
+        {
+            get { return firstName; }
+        }
+
+        public string SecondName
+        {
+            get { return secondName; }
+        }
+
+        public int Expirience
+        {
+            get { return expirience; }
+            set
+            {
+                if(value>=0)
+                expirience = value;
+            }
+        }
+
+        public string Post
+        {
+            get
+            {
+                if (post == null) 
+                    return "Должность неизвестна";
+                return post;
+            }
+            set
+            {
+                if (value!=null)
+                post = value;
+            }
+        }
         public Employee (string firstName,string secondName)
         {
             this.firstName = firstName;
             this.secondName = secondName;
         }
 
-        private 
+
+
+   
     }
 }

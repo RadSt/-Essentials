68a8ba4abe5dfb6caaf928c6967ec9cae5be7304
RadSt <desan1986@gmail.com>
2015-06-14 20:59:50 +0300

    Проверка коммита

---
+++ Classes2/Tack3/Tack3/Tack3/Employee.cs
@@ -0,0 +1,18 @@
+﻿namespace Tack3
+{
+    public class Employee
+    {
+        readonly string firstName, secondName;
+        private int expirience;
+        private string post;
+        
+
+        public Employee (string firstName,string secondName)
+        {
+            this.firstName = firstName;
+            this.secondName = secondName;
+        }
+
+        private 
+    }
+}

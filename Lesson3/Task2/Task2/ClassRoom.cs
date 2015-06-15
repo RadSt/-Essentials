4a517750ffac74fcbd989c38312d65f12be128c5
RadikSt <desan1986@gmail.com>
2015-06-15 16:58:51 +0300

    end work

---
+++ Lesson3/Task2/Task2/ClassRoom.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace Task2
+{
+    public class ClassRoom
+    {
+        Random rand=new Random();
+        Pupil[] pupils = new Pupil[4];
+
+        public ClassRoom(Pupil p0)
+        {
+            pupils[0] = p0;
+            pupils[1] = GeneratePupil();
+        }
+        public ClassRoom(Pupil p1,Pupil p2)
+        {
+            this.pupil1 = pupil1;
+            this.pupil2 = pupil2;
+        }
+        public ClassRoom(Pupil p1,Pupil p2,Pupil p3)
+        {
+            this.pupil1 = pupil1;
+            this.pupil2 = pupil2;
+            this.pupil3 = pupil3;
+        }
+        public ClassRoom(Pupil p1,Pupil p2,Pupil p3,Pupil p4)
+        {
+            this.pupil1 = pupil1;
+            this.pupil2 = pupil2;
+            this.pupil3 = pupil3;
+            this.pupil4 = pupil4;
+        }
+
+        private Pupil GeneratePupil()
+        {
+            int r = rand.Next(1, 4);
+            switch (r)
+            {
+                case 1: return new ExcelentPupil();
+            }
+        }
+    }
+}
---
+++ Lesson3/Task2/Task2/Pupil.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task2
+{
+    public class Pupil
+    {
+        public virtual void Study()
+        {
+            Console.WriteLine("Pupil.Study()");
+        }
+
+        public virtual void Read()
+        {
+            Console.WriteLine("Pupil.Read()");
+        }
+
+        public virtual void Write()
+        {
+            Console.WriteLine("Pupil.Write()");
+        }
+
+        public virtual void Relax()
+        {
+            Console.WriteLine("Pupil.Relax()");
+        }
+    }
+}

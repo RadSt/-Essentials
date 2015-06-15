6829d8fb5588d386dd92bb2589ce518a5e4bc3d5
RadikSt <desan1986@gmail.com>
2015-06-15 09:28:44 +0300

    les 2 task4 end

---
+++ Class2/Task4/Task4/Task4/Invoice.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Task4
+{
+    public class Invoice
+    {
+        protected int account;
+        protected string customer, provider;
+        private string article;
+        private int quantity;
+
+        public int Account { get { return account; }}
+        public string Customer { get { return customer; } }
+        public string Provider { get { return provider; }}
+
+        public string Article
+        {
+            get
+            {
+                if (article == null)
+                    return "Неизвестный товар";
+                return article;
+            } 
+            set
+            {
+                if (value != null)
+                    article = value;
+            }
+        }
+
+        public int Quantity
+        {
+            get { return quantity; }
+            set
+            {
+                if (value >= 0)
+                    quantity = value;
+            }
+        }
+
+        public Invoice (int account, string customer,string provider)
+        {
+            this.account = account;
+            this.customer = customer;
+            this.provider = provider;
+        }
+
+        public void CountCost(bool need)
+        {
+            double cost;
+            switch (article.ToLower())
+            {
+                case "яблоко": cost = 20;
+                    break;
+                case "апельсин": cost = 30;
+                    break;
+                case "ананас": cost = 40;
+                    break;
+                case "мандарин": cost = 50;
+                    break;
+                default: cost = 0;
+                    break;
+            }
+            if (need)
+            {
+                cost = cost+cost * 0.13;
+            }
+            Console.WriteLine("Сумма к оплате {0}",cost*quantity);
+        }
+
+    }
+}

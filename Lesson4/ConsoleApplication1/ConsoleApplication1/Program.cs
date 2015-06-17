9b751d9ae1345eb797d1b013919a06e77b17d24c
RadikSt <desan1986@gmail.com>
2015-06-17 16:13:33 +0300

    awdawd

--- Lesson4/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ Lesson4/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Redactor redactor=new Redactor();
-            redactor.ChooseDocument("letter.xml");
+            redactor.ChooseDocument("letter.txt");
             redactor.Create();
             redactor.Open();
             redactor.Change();

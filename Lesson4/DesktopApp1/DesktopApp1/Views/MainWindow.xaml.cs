d77159cae0fd50c024042fbd485369f094663044
RadikSt <desan1986@gmail.com>
2015-08-01 13:51:32 +0300

    Stable

---
+++ Lesson4/DesktopApp1/DesktopApp1/Views/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MVVMbasics.Attributes;
+using Core.Viewmodels;
+using MVVMbasics.Views;
+
+namespace DesktopApp1.Views
+{
+    [MvvmNavigationTarget(typeof(MainViewmodel))]
+    public partial class MainWindow : BaseView
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}

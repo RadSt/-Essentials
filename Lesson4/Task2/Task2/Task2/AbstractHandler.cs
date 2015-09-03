63d8619b3d3b1542ec5c21728f408887eacea218
RadikST <desan1986@gmail.com>
2015-09-03 22:01:27 +0300

    Starting work after a loooooong break)))

---
+++ Lesson4/Task2/Task2/Task2/AbstractHandler.cs
@@ -0,0 +1,10 @@
+﻿namespace Task2
+{
+    public abstract class AbstractHandler
+    {
+        public abstract void Open();
+        public abstract void Create();
+        public abstract void Change();
+        public abstract void Save();
+    }
+}

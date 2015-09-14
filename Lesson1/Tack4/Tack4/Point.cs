84167362823ddb2b02669a8467a771b27f1a3e01
RadST <desan1986@gmail.com>
2015-09-14 20:01:35 +0300

    End of les 1

---
+++ Lesson1/Tack4/Tack4/Point.cs
@@ -0,0 +1,18 @@
+﻿namespace Tack4
+{
+    public class Point
+    {
+        private int x;
+        private int y;
+        private string name;
+        public Point(string name,int x,int y)
+        {
+            this.x = x;
+            this.y = y;
+            this.name = name;
+        }
+        public int X { get; }
+        public int Y { get; }
+        public string Name { get; }
+    }
+}

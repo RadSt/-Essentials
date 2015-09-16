b5765ffb6b3629874dc09a5f905b9630e8c61468
RadST <desan1986@gmail.com>
2015-09-16 21:57:02 +0300

    end of les3 tack1

---
+++ Lesson3/Task1/Task1/Colour.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Task1
+{
+    public class Colour:Printer
+    {
+        public Colour(ConsoleColor consoleColor)
+            :base(consoleColor)
+        {
+            
+        }
+    }
+}
---
+++ Lesson3/Task1/Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer blue = new Colour(ConsoleColor.Blue);
+            blue.Print("Hello");
+            Printer yellow =new Colour(ConsoleColor.Yellow);
+            yellow.Print("MyFriend");
+            Console.ReadKey();
+        }
+    }
+}

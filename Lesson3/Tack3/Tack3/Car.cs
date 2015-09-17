00f50145a46018ed17207dd463e1e1a62abeead8
RadST <desan1986@gmail.com>
2015-09-17 22:02:13 +0300

    End les3 tack3

---
+++ Lesson3/Tack3/Tack3/Car.cs
@@ -0,0 +1,18 @@
+﻿namespace Tack3
+{
+    public class Car:Vehicle
+    {
+        public Car(int xLocation, int yLocation, int price, int speed, int year)
+            : base(xLocation, yLocation, price, speed, year)
+        {
+            
+        }
+
+        public Car(int price, int speed, int year)
+            : base(price, speed, year)
+        {
+            
+        }
+
+    }
+}
---
+++ Lesson3/Tack3/Tack3/Vehicle.cs
@@ -0,0 +1,30 @@
+﻿namespace Tack3
+{
+    public class Vehicle
+    {
+        private int xLocation;
+        private int yLocation;
+        private int price;
+        private int speed;
+        private int year;
+        public Vehicle(int xLocation, int yLocation, int price, int speed, int year)
+        {
+            this.xLocation = xLocation;
+            this.yLocation = yLocation;
+            this.price = price;
+            this.speed = speed;
+            this.year = year;
+        }
+
+        public Vehicle(int price, int speed, int year)
+            :this(0,0,price,speed,year)
+        {
+            
+        }
+        public int XLocation { get { return xLocation; } }
+        public int YLocation { get { return yLocation; } }
+        public int Price { get { return price; } }
+        public int Speed { get { return speed; } }
+        public int Year { get { return year; } }
+    }
+}

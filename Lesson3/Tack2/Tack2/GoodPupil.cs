0da0cc12ec2178656f67b3c9ecfcb50ad016e004
RadST <desan1986@gmail.com>
2015-09-17 21:13:24 +0300

    End les 3 tack2

---
+++ Lesson3/Tack2/Tack2/GoodPupil.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Tack2
+{
+    public class GoodPupil:Pupil
+    {
+        public override void Study()
+        {
+            Console.WriteLine("GoodPupil Study");
+        }
+
+        public override void Read()
+        {
+            Console.WriteLine("GoodPupil Read");
+        }
+
+        public override void Write()
+        {
+            Console.WriteLine("GoodPupil Write");
+        }
+        public override void Relax()
+        {
+            Console.WriteLine("GoodPupil Relax");
+        }
+    }
+}

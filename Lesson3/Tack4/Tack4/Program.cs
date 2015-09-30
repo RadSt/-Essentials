6ac09b1266024f97fe173ef6546dcf41d13f859a
RadST <desan1986@gmail.com>
2015-09-30 21:47:25 +0300

    end of les 3 tack 4

---
+++ Lesson3/Tack4/Tack4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tack4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Redaktor redaktor=new Redaktor();
+            Console.WriteLine("Введите ключь");
+            redaktor.Key=Console.ReadLine();
+
+            redaktor.ChooseLisense();
+            redaktor.Open();
+            redaktor.Edit();
+            redaktor.Save();
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Lesson3/Tack4/Tack4/Redaktor.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+
+namespace Tack4
+{
+    public class Redaktor
+    {
+        private string key;
+        private DocumentWorker documentWorker;
+
+        public string Key
+        {
+            set { key = value; }
+        }
+
+        public void ChooseLisense()
+        {
+            switch (key)
+            {
+                case "pro":
+                    documentWorker = new ProDocumentWorker();
+                    break;
+                case "exp":
+                    documentWorker = new ExpertDocumentWorker();
+                    break;
+                default:
+                    documentWorker = new DocumentWorker();
+                    break;
+            }
+        }
+
+        public void Open()
+        {
+            documentWorker.OpenDocument();
+        }
+
+        public void Save()
+        {
+            documentWorker.SaveDocument();
+        }
+
+        public void Edit()
+        {
+            documentWorker.EditDocument();
+        }
+
+    }
+}

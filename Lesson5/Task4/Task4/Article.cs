d7e5558579d32cd5a879a0c01e8cc874a8432935
RadST <desan1986@gmail.com>
2015-10-04 21:49:10 +0300

    cont tom task 4

---
+++ Lesson5/Task4/Task4/Article.cs
@@ -0,0 +1,16 @@
+﻿namespace Task4
+{
+    public class Article
+    {
+        public string ItemName { get; set; }
+        public string ShopName { get; set; }
+        public decimal Price { get; set; }
+
+        public Article(string itemName, string shopName, int price)
+        {
+            ItemName = itemName;
+            ShopName = shopName;
+            Price = price;
+        }
+    }
+}
---
+++ Lesson5/Task4/Task4/Store.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task4
+{
+    public class Store
+    {
+        private Article[] article;
+
+        Store()
+        {
+            article[0]=new Article("Яблоко","Магнит",10);
+            article[1] = new Article("Гречка", "Эссен", 20);
+            article[2] = new Article("Макароны", "Камилла", 30);
+            article[3] = new Article("Лапша", "Челны Хлеб", 40);
+        }
+
+        public string this[int index]
+        {
+            get
+            {
+                for (int i = 0; i < article.Length; i++)
+                    if (article[i]==index)
+                    Console.WriteLine(article[i].ItemName);
+            }
+        }
+
+
+    }
+}

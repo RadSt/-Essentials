0b273f2a3f9cf6eeda2afb0414fe8bc6f2cfa2a5
RadST <desan1986@gmail.com>
2015-10-05 20:53:24 +0300

    end les 6 task 1

---
+++ Lesson6/Task1/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calculator.Add(1,2));
+            Console.WriteLine(Calculator.Multiply(2,3));
+            Console.WriteLine(Calculator.Split(4,2));
+            Console.WriteLine(Calculator.Substract(6,3));
+
+            Console.ReadKey();
+        }
+    }
+}

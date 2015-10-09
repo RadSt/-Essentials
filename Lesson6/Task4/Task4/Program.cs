d34859183a4184b418130b0f4bcc4e104573e7c5
RadikSt <desan1986@gmail.com>
2015-10-09 10:12:58 +0300

    Mod les 6 task 4 add new ext method

--- Lesson6/Task4/Task4/Program.cs
+++ Lesson6/Task4/Task4/Program.cs
@@ -6,22 +6,18 @@ using System.Threading.Tasks;
 
 namespace Task4
 {
-    static class Arraysort
+    static class ArraysortAndShow
     {
         public static void Sorting(this int[] intArray)
         {
+            Array.Sort(intArray);
+        }
+
+        public static void ShowArrayInConsole(this int[] intArray)
+        {
             for (int i = 0; i < intArray.Length; i++)
             {
-                for (int j = intArray.Length - 1; j > i; j--)
-                {
-                    if (intArray[i] > intArray[j])
-                    {
-                        int x = intArray[j];
-                        intArray[j] = intArray[i];
-                        intArray[i] = x;
-                    }
-
-                }
+                Console.Write(intArray[i] + " ");
             }
         }
     }
@@ -31,15 +27,13 @@ namespace Task4
         private static void Main(string[] args)
         {
             int[] intArray = {1, 4, 2, 5, 6, 10, 8, 9, 3, 7};
-            Arraysort.Sorting(intArray);
+            ArraysortAndShow.Sorting(intArray);
             intArray.Sorting();
+            intArray.ShowArrayInConsole();
 
-            for (int i = 0; i < intArray.Length; i++)
-            {
-                Console.Write(intArray[i] + " ");
-            }
 
             Console.ReadKey();
         }
+
     }
 }

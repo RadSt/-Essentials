6714e034f466df256187a24fd2b2b698b54d2a9e
RadST <desan1986@gmail.com>
2015-10-19 21:27:25 +0300

    End of les 8 task 2

---
+++ Lesson8/Task2/Task2/PrintString.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task2
+{
+    public static class PrintString
+    {
+        public static void Print(int color, string text)
+        {
+            switch (color)
+            {
+                case (int)ColorsEnum.Blue:
+                    Console.ForegroundColor=ConsoleColor.Blue;
+                    break;
+                case (int)ColorsEnum.Green:
+                    Console.ForegroundColor=ConsoleColor.Green;
+                    break;
+                case (int)ColorsEnum.Red:
+                    Console.ForegroundColor=ConsoleColor.Red;
+                    break;
+                case (int)ColorsEnum.Cyan:
+                    Console.ForegroundColor=ConsoleColor.Cyan;
+                    break;
+                case (int)ColorsEnum.Yellow:
+                    Console.ForegroundColor=ConsoleColor.Yellow;
+                    break;
+                default:
+                    Console.ForegroundColor=ConsoleColor.Gray;
+                    break;
+            }
+            Console.WriteLine(text);
+        }
+    }
+}

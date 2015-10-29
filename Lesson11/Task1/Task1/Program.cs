0eb67c380a85e5ea4f76e786be479a41bc2dabaa
RadST <desan1986@gmail.com>
2015-10-29 22:07:43 +0300

    End of les 11 task 1

---
+++ Lesson11/Task1/Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list=new ArrayList();
+            list.Add(0);
+            list.Add(0.25);
+            list.Add('а');
+            list.Add("Hello");
+            list.Add(new MyClass());
+
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

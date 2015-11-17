36958d52c44179751cbc05ba9530b9c042e88ed1
RadST <desan1986@gmail.com>
2015-11-17 22:41:09 +0300

    End of les 11 task 2

--- Lesson11/Task2/Task2/CarCollection.cs
+++ Lesson11/Task2/Task2/CarCollection.cs
@@ -1,36 +1,46 @@
-﻿namespace Task2
+﻿using System;
+using System.Net.Sockets;
+
+namespace Task2
 {
-    public class CarCollection<T> where T:class
+    public class CarCollection<T> : MyList<T> 
     {
-        private T[] cars;
-        public CarCollection(int n)
+        readonly MyList<string> carName;
+        readonly MyList<DateTime> carYear;
+
+        public CarCollection()
         {
-            cars=new T[0];
+            carName=new MyList<string>();
+            carYear=new MyList<DateTime>();
         }
 
-        public void Add(T entity)
+        public void AddCar(string name, DateTime year)
         {
-            var temp = new T[cars.Length + 1];
-            for (int i = 0; i < temp.Length; i++)
+            carName.Add(name);
+            carYear.Add(year);
+        }
+
+        public new string this[int index]
+        {
+            get { return carName[index] + " " + carYear[index].Year; }
+        }
+
+        public int Length { get { return carName.Count; } }
+
+        public void Remove()
+        {
+            carName.Clear();
+            carYear.Clear();
+        }
+
+        public override string ToString()
+        {
+            string stroka = null;
+            for (int i = 0; i < carName.Count; i++)
             {
-                temp[i] = entity;
+                 stroka += "№" + (i + 1) + " " + carName[i] + " " + carYear[i].Year + " г \n";
             }
-            cars = temp;
-        }
-
-        public T this [int index]
-        {
-            get
-            {
-                return cars[index];
-            }
-        }
-
-        public int Count { get { return cars.Length; } }
-
-        public void Clear()
-        {
-            cars=new T[0];
+            return stroka ?? "В парке нет машин";
         }
     }
 }
--- Lesson11/Task2/Task2/Program.cs
+++ Lesson11/Task2/Task2/Program.cs
@@ -6,21 +6,23 @@ namespace Task2
     {
         static void Main(string[] args)
         {
-            Car car=new Car();
-            car.Name = "Mersedes";
-            car.Year = "1985";
-            int razm = 10;
+            Console.WriteLine("Наш парк машин");
+            var park=new CarCollection<string>();
 
-            CarCollection<Car> carCollection=new CarCollection<Car>(razm);
+            park.AddCar("Жигули", new DateTime(1985, 12, 5));
+            park.AddCar("Тойота", new DateTime(2000, 4, 7));
+            park.AddCar("Форд", new DateTime(2005, 5, 3));
+            park.AddCar("Мерседес", new DateTime(2003, 11, 3));
+            Console.WriteLine(park.ToString());
 
-            for (int i = 0; i < razm; i++)
+            Console.WriteLine("В парке находится {0} машин",park.Length);
+
+            Console.WriteLine("Введите номер интересующей вас машины.");
+            string str=Console.ReadLine();
+        
+            if (!string.IsNullOrEmpty(str))
             {
-                carCollection.Add(car);
-            }
-
-            for (int i = 0; i < carCollection.Count; i++)
-            {
-                Console.WriteLine(carCollection[i].Name+" "+carCollection[i].Year+" ");
+                Console.WriteLine(park[Convert.ToInt32(str) - 1]);
             }
 
             Console.ReadKey();

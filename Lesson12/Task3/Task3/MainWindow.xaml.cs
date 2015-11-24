d6c4ee2013b04c3e3bc7f7d0a9000ac95abd0376
RadST <desan1986@gmail.com>
2015-11-24 22:04:54 +0300

    Work on les 12 task 3

---
+++ Lesson12/Task3/Task3/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows;
+
+namespace Task3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            new Presenter();
+        }
+        public event EventHandler Minus=null;
+        public event EventHandler Umnozit = null;
+        public event EventHandler Delit = null;
+        public event EventHandler Plus = null;
+        public event EventHandler Result = null;
+        private void BtnMinus_Click(object sender, RoutedEventArgs e)
+        {
+            Minus.Invoke(sender,e);
+        }
+
+        private void BtnUmnozit_Click(object sender, RoutedEventArgs e)
+        {
+            Umnozit.Invoke(sender,e);
+        }
+
+        private void BtnPlus_Click(object sender, RoutedEventArgs e)
+        {
+            Plus.Invoke(sender,e);
+        }
+
+        private void BtnDelit_Click(object sender, RoutedEventArgs e)
+        {
+            Delit.Invoke(sender,e);
+        }
+
+        private void BtnResult_Click(object sender, RoutedEventArgs e)
+        {
+            Result.Invoke(sender,e);
+        }
+    }
+}
---
+++ Lesson12/Task3/Task3/Presenter.cs
@@ -0,0 +1,44 @@
+﻿namespace Task3
+{
+    public class Presenter
+    {
+        Model model;
+        MainWindow mainWindow;
+
+        public Presenter(MainWindow mainWindow)
+        {
+            this.model=new Model();
+            this.mainWindow = mainWindow;
+            this.mainWindow.Delit += MainWindow_Delit;
+            this.mainWindow.Umnozit += MainWindow_Umnozit;
+            this.mainWindow.Plus += MainWindow_Plus;
+            this.mainWindow.Minus += MainWindow_Minus;
+            this.mainWindow.Result += MainWindow_Result;
+        }
+
+        private void MainWindow_Result(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void MainWindow_Minus(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void MainWindow_Plus(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void MainWindow_Umnozit(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void MainWindow_Delit(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

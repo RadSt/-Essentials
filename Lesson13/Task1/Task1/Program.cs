bdf448b34974f300599b319048d3555b945154c3
RadST <desan1986@gmail.com>
2015-11-25 20:30:54 +0300

    End of les 13 task 1

--- Lesson13/Task1/Task1/Program.cs
+++ Lesson13/Task1/Task1/Program.cs
@@ -2,14 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Task1
 {
     class Program
     {
+        private static int count;
+        static void NewThread()
+        {
+            Console.WriteLine("Say Hello {0}", Thread.CurrentThread.Name);
+            Thread.Sleep(1000);
+            Thread thread = new Thread(NewThread);
+            count++;
+            thread.Name = "Thread " + count;
+            thread.Start();
+            thread.IsBackground = true;
+        }
         static void Main(string[] args)
         {
+            Thread thread = new Thread(NewThread) {Name = "Thread "+count};
+            thread.Start();
+            thread.IsBackground = true;
+
+            Console.ReadLine();
         }
     }
 }

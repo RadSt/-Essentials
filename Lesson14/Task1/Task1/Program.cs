fd34223e7bf30500a84ee776592c04e90c61ac58
RadST <desan1986@gmail.com>
2015-11-26 22:44:04 +0300

    End of less 14 task 1

--- Lesson14/Task1/Task1/Program.cs
+++ Lesson14/Task1/Task1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,31 @@ namespace Task1
     {
         static void Main(string[] args)
         {
+            Random rand=new Random();
+            int[] arrint=new int[10];
+
+            for (int i = 0; i < arrint.Length; i++)
+            {
+                arrint[i] = rand.Next(50);
+            }
+            foreach (var elem in Nechet(arrint))
+            {
+                Console.WriteLine(elem);
+            }
+            Console.ReadLine();
+        }
+
+        static IEnumerable Nechet(int[] arrInt)
+        {
+            for (int i = 0; i < arrInt.Length; i++)
+            {
+                if (arrInt[i]%2==0)
+                {
+                    yield return arrInt[i];
+                }
+                
+            }
+            
         }
     }
 }

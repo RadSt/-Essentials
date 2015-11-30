a9bbf5ed7cbac1a5da7be529520ae1a859672e9f
RadikSt <desan1986@gmail.com>
2015-11-30 15:16:57 +0300

    End of les 15 task 2

--- Lesson15/Task 2/Task 2/Program.cs
+++ Lesson15/Task 2/Task 2/Program.cs
@@ -10,6 +10,38 @@ namespace Task_2
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Worker[] worker = new Worker[2];
+
+                for (int i = 0; i < 2; i++)
+                {
+                    Console.WriteLine("Введите ФИО");
+                    worker[i].Fio = Console.ReadLine();
+                    Console.WriteLine("Введите Должность");
+                    worker[i].Post = Console.ReadLine();
+                    Console.WriteLine("Введите год принятия");
+                    worker[i].EmploymentYear = DateTime.Parse(Console.ReadLine());
+                }
+                Console.WriteLine("Введите стаж работника");
+                int stag = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = 0; i < worker.Length; i++)
+                {
+                    if (Convert.ToInt32(DateTime.Now.Year-worker[i].EmploymentYear.Year)>stag)
+                    {
+                        Console.WriteLine("ФИО: {0} \n Должность: {1} \n Год принятия: {2} \n",
+                            worker[i].Fio,worker[i].Post,worker[i].EmploymentYear);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Ошибка {0} ",ex);
+            }
+
+            Console.ReadLine();
         }
     }
 }
---
+++ Lesson15/Task 2/Task 2/Worker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task_2
+{
+    public struct Worker
+    {
+        public string Fio;
+        public string Post;
+        public DateTime EmploymentYear;
+
+    }
+}

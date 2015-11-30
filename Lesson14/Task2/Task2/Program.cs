aacd6c356d1155c38293d40324c46d69082dba65
RadikSt <desan1986@gmail.com>
2015-11-30 07:44:04 +0300

    End less 14 task 2

--- Lesson14/Task2/Task2/Program.cs
+++ Lesson14/Task2/Task2/Program.cs
@@ -10,6 +10,21 @@ namespace Task2
     {
         static void Main(string[] args)
         {
+            var collection =new MyListCollection<string>();
+
+            collection.Add("Привет");
+            collection.Add("Пока");
+            collection.Add("Смотри");
+            collection.Add("На");
+            collection.Add("Меня");
+            collection.Add("Внимательно");
+
+            foreach (var elem in collection)
+            {
+                Console.WriteLine(elem);
+            }
+
+            Console.ReadKey();
         }
     }
 }

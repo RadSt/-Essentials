7bcfc0fa450f77160caf14d32edc89ef265dce92
RadikSt <desan1986@gmail.com>
2015-11-30 11:34:56 +0300

    Add elems from 2 task

---
+++ Lesson14/Task4/Task4/MyListCollection.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Task4
+{
+    public class MyListCollection<T> : IEnumerable<T>
+    {
+        int position = -1;
+        private T[] elementsArray;
+
+        public MyListCollection()
+        {
+            elementsArray = new T[0];
+        }
+        public int Count
+        {
+            get { return elementsArray.Length; }
+        }
+        T this[int index]
+        {
+            get { return elementsArray[index]; }
+        }
+        public void Add(T elem)
+        {
+            T[] temp = new T[Count + 1];
+            for (int i = 0; i < elementsArray.Length; i++)
+            {
+                temp[i] = elementsArray[i];
+            }
+            temp[Count] = elem;
+            elementsArray = temp;
+        }
+        public void Reset()
+        {
+            position = -1;
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            while (true)
+            {
+                if (position < elementsArray.Length - 1)
+                {
+                    position++;
+                    yield return elementsArray[position];
+
+                }
+                else
+                {
+                    Reset();
+                    yield break;
+                }
+            }
+        }
+    }
+}
--- Lesson14/Task4/Task4/Program.cs
+++ Lesson14/Task4/Task4/Program.cs
@@ -10,6 +10,21 @@ namespace Task4
     {
         static void Main(string[] args)
         {
+            var collection = new MyListCollection<string>();
+
+            collection.Add("Привет");
+            collection.Add("Пока");
+            collection.Add("Смотри");
+            collection.Add("На");
+            collection.Add("Меня");
+            collection.Add("Внимательно");
+
+            foreach (var elem in collection)
+            {
+                Console.WriteLine(elem);
+            }
+
+            Console.ReadKey();
         }
     }
 }

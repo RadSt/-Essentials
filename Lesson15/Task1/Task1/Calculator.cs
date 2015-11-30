3f810ed70e83095968c5bd838c07542abb8db3b7
RadikSt <desan1986@gmail.com>
2015-11-30 13:29:58 +0300

    End les 15 task 1

---
+++ Lesson15/Task1/Task1/Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task1
+{
+    public class Calculator
+    {
+        public string Add(int num1, int num2)
+        {
+            return (num1 + num2).ToString();
+        }
+
+        public string Sub(int num1, int num2)
+        {
+            return (num1 - num2).ToString();
+        }
+
+        public string Mul(int num1, int num2)
+        {
+            return (num1*num2).ToString();
+        }
+
+        public string Div(int num1, int num2)
+        {
+            try
+            {
+                return (num1/num2).ToString();
+            }
+            catch (DivideByZeroException)
+            {
+                
+                return "На ноль делить нельзя";
+            }
+        }
+    }
+}

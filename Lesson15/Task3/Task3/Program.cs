2793ddb92aac7fe57abbb9a0721bff1d08011fd8
RadikSt <desan1986@gmail.com>
2015-11-30 16:51:57 +0300

    End of working day

--- Lesson15/Task3/Task3/Program.cs
+++ Lesson15/Task3/Task3/Program.cs
@@ -6,16 +6,49 @@ using System.Threading.Tasks;
 
 namespace Task3
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Product[] products=new Product[2];
+            var products = new Product[2];
+            Exception ex = new Exception("Данный магазин не найден");
+            int i = 0;
 
-            for (int i = 0; i < products.Length; i++)
+            try
             {
-                
+                for (int i = 0; i < products.Length; i++)
+                {
+                    Console.WriteLine("Введите название товара");
+                    products[i].Name = Console.ReadLine();
+                    Console.WriteLine("Введите имя магазина");
+                    products[i].ShopName = Console.ReadLine();
+                    Console.WriteLine("Введите цену");
+                    products[i].Price = Convert.ToDecimal(Console.ReadLine());
+                }
+
+                var sortedProducts = products.OrderBy(s => s.ShopName).ToArray();
+
+                Console.WriteLine("Введите название товара");
+                string productShopName = Console.ReadLine();
+
+                for ( i < sortedProducts.Length; i++)
+                {
+                    if (sortedProducts[i].ShopName == productShopName)
+                    {
+                        Console.WriteLine("В этом магазине есть {0} ", sortedProducts[i].Name);
+                    }
+                    else
+                    {
+                        throw ex;
+                    }
+                }
             }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc);
+            }
+
+            Console.ReadLine();
         }
     }
 }

18dc81ee2c83389bbca2c9f0ab45bd12d056646f
RadikSt <desan1986@gmail.com>
2015-12-01 10:42:32 +0300

    Add Company class for les 15 task 2 small refs clas logic

--- Lesson15/Task 2/Task 2/Program.cs
+++ Lesson15/Task 2/Task 2/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Task_2
 {
@@ -10,40 +6,11 @@ namespace Task_2
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Worker[] worker = new Worker[2];
-
-                for (int i = 0; i < worker.Length; i++)
-                {
-                    Console.WriteLine("Введите ФИО");
-                    worker[i].Fio = Console.ReadLine();
-                    Console.WriteLine("Введите Должность");
-                    worker[i].Post = Console.ReadLine();
-                    Console.WriteLine("Введите год принятия");
-                    worker[i].EmploymentYear = DateTime.Parse(Console.ReadLine());
-                }
-                var sortedWorker=worker.OrderBy(s => s.Fio).ToArray();
-
-                Console.WriteLine("Введите стаж работника");
-                int stag = Convert.ToInt32(Console.ReadLine());
-
-                for (int i = 0; i < sortedWorker.Length; i++)
-                {
-                    if (Convert.ToInt32(DateTime.Now.Year - sortedWorker[i].EmploymentYear.Year) > stag)
-                    {
-                        Console.WriteLine("ФИО: {0} \n Должность: {1} \n Год принятия: {2} \n",
-                            sortedWorker[i].Fio, sortedWorker[i].Post, sortedWorker[i].EmploymentYear);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-
-                Console.WriteLine("Ошибка {0} ",ex);
-            }
+            var company=new Company();
+            Console.WriteLine(company[5]);
 
             Console.ReadLine();
+
         }
     }
 }
--- Lesson15/Task 2/Task 2/Worker.cs
+++ Lesson15/Task 2/Task 2/Worker.cs
@@ -4,9 +4,25 @@ namespace Task_2
 {
     public struct Worker
     {
-        public string Fio;
-        public string Post;
-        public DateTime EmploymentYear;
+        private string fio;
+        private string post;
+        private int employmentYear;
+
+        public string Fio { get { return fio; }  }
+        public string Post { get { return post; }  }
+        public int EmploymentYear { get { return employmentYear; } }
+
+        public Worker(string fio, string post, int employmentYear)
+        {
+            this.fio = fio;
+            this.post = post;
+            this.employmentYear = employmentYear;
+        }
+
+        public string Show()
+        {
+            return String.Format("ФИО: {0} \n Должность: {1} \n Год принятия: {2} \n",fio,post,employmentYear);
+        }
 
     }
 }

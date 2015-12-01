035462fec7c72e8fe3d42d55bcfc3038e8dc6ca4
RadikSt <desan1986@gmail.com>
2015-12-01 13:13:11 +0300

    End less 16 task 1

--- Lesson16/Task1/Task1/Program.cs
+++ Lesson16/Task1/Task1/Program.cs
@@ -10,6 +10,14 @@ namespace Task1
     {
         static void Main(string[] args)
         {
+            var p1 = new Point(1, 2, 3);
+            var p2 = new Point(4, 5, 6);
+
+            var p3 = p1 + p2;
+
+            Console.WriteLine("X {0} Y {1} Z {2}",p3.X,p3.Y,p3.Z);
+
+            Console.ReadLine();
         }
     }
 }

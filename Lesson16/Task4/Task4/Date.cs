e82a8360681c98033c2c9648f160d8c265a3d754
RadikSt <desan1986@gmail.com>
2015-12-02 16:59:38 +0300

    End Work

--- Lesson16/Task4/Task4/Date.cs
+++ Lesson16/Task4/Task4/Date.cs
@@ -1,7 +1,27 @@
-﻿namespace Task4
+﻿using System;
+using System.CodeDom;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+
+namespace Task4
 {
     public class Date
     {
-         
+        private int day, month,year;
+
+        public Date(DateTime dat)
+        {
+            this.day = dat.Day;
+            this.month = dat.Month;
+            this.year = dat.Year;
+        }
+
+        public int Day { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+
+        public static int operator -(DateTime dat1, DateTime dat2)
+        {
+            
+        }
     }
 }
